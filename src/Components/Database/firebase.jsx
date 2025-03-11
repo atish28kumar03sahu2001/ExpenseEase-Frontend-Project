3ee9f1cc2ec6b19f58e08ca63de08d8c5929bcdc
Atish Kumar Sahu <160933920+atish28kumar03sahu2001@users.noreply.github.com>
2025-03-11 11:29:03 +0530

    Final Commit

--- src/Components/Database/firebase.jsx
+++ src/Components/Database/firebase.jsx
@@ -1,13 +1,13 @@
 // src/Components/Database/firebase.jsx
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyC-mD4DBs9eXVFiKt1ufLn3LDXJYKK9LyY",
-  authDomain: "expenseeasereact.firebaseapp.com",
-  databaseURL: "https://expenseeasereact-default-rtdb.firebaseio.com",
-  projectId: "expenseeasereact",
-  storageBucket: "expenseeasereact.firebasestorage.app",
-  messagingSenderId: "756336475857",
-  appId: "1:756336475857:web:87c3647aa7cb41aabbcff0",
-  measurementId: "G-6CQMCY358C"
+  apiKey: "API_AUTH_KEY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABSE_URL",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "API_ID",
+  measurementId: "MEASUREMENT_ID"
 };
 export const app = initializeApp(firebaseConfig);
